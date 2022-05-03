1ae2dc64b7138c2e81ee425856f87460bec6ff9b
Abhay Kumar <abhay.kumar@wisc.edu>
2022-05-03 02:46:57 -0500

    updated MTL code

--- run_multi_task.py
+++ run_multi_task.py
@@ -171,6 +171,37 @@ class GoemotionProcessor(DataProcessor):
                 InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
         return examples
 
+class GoemotionProcessor_ekman(DataProcessor):
+    """Processor for the GoemotionProcessor data set."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1", "2", "3", "4", "5", "6"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            text_a = tokenization.convert_to_unicode(line[0])
+            label = tokenization.convert_to_unicode(line[1])
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
+
+
 def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer, task='none'):
     """Loads a data file into a list of `InputBatch`s."""
 
@@ -451,7 +482,9 @@ def main():
 
     processors = {
         "sst": SSTProcessor,
-        "goemotion" : GoemotionProcessor
+        "goemotion" : GoemotionProcessor,
+        "goemotion_original": GoemotionProcessor,
+        "goemotion_ekman" : GoemotionProcessor_ekman
     }
 
     if args.local_rank == -1 or args.no_cuda:
@@ -494,6 +527,9 @@ def main():
     if args.tasks == 'all':
         task_names =['sst', 'goemotion']
         data_dirs = ['SST-2', 'Goemotion']
+    elif args.tasks == 'goemotion_taxonomy':
+        task_names =['goemotion_original', 'goemotion_ekman']
+        data_dirs = ['mtl_original', 'mtl_ekman']
     elif args.tasks == 'single':
         task_names = ['sst', 'goemotion']
         data_dirs = ['SST-2', 'Goemotion']
