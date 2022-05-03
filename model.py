5adc4cac87844ba9feccebcbf264058f5b2ef72c
Abhay Kumar <abhay.kumar@wisc.edu>
2022-05-02 22:40:31 -0500

    updated code

--- model.py
+++ model.py
@@ -3,7 +3,11 @@
 # To support multi-class classification in Goemotions classification task
 #########################################
 import torch.nn as nn
+import numpy as np
+from typing import Union, Optional
 from transformers import BertPreTrainedModel, BertModel
+from transformers.pipelines import ArgumentHandler
+from transformers import Pipeline, PreTrainedTokenizer, ModelCard
 
 
 class BertForMultiLabelClassification(BertPreTrainedModel):
@@ -48,3 +52,47 @@ class BertForMultiLabelClassification(BertPreTrainedModel):
             outputs = (loss,) + outputs
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
+
+
+
+
+# class MultiLabelPipeline(Pipeline):
+#     def __init__(
+#             self,
+#             model: Union["PreTrainedModel", "TFPreTrainedModel"],
+#             tokenizer: PreTrainedTokenizer,
+#             modelcard: Optional[ModelCard] = None,
+#             framework: Optional[str] = None,
+#             task: str = "",
+#             args_parser: ArgumentHandler = None,
+#             device: int = -1,
+#             binary_output: bool = False,
+#             threshold: float = 0.3
+#     ):
+#         super().__init__(
+#             model=model,
+#             tokenizer=tokenizer,
+#             modelcard=modelcard,
+#             framework=framework,
+#             args_parser=args_parser,
+#             device=device,
+#             binary_output=binary_output,
+#             task=task
+#         )
+
+#         self.threshold = threshold
+
+#     def __call__(self, *args, **kwargs):
+#         outputs = super().__call__(*args, **kwargs)
+#         scores = 1 / (1 + np.exp(-outputs))  # Sigmoid
+#         results = []
+#         for item in scores:
+#             labels = []
+#             scores = []
+#             for idx, s in enumerate(item):
+#                 if s > self.threshold:
+#                     labels.append(self.model.config.id2label[idx])
+#                     scores.append(s)
+#             results.append({"labels": labels, "scores": scores})
+#         return results
+
